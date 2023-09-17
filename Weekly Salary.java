7d9fb2f17c2a33d1dae9334b01c35dc09841cf4c
SanchoAshley47 <143739118+SanchoAshley47@users.noreply.github.com>
2023-09-17 15:44:49 -0400

    Create Weekly Salary.java

---
+++ Weekly Salary.java
@@ -0,0 +1,20 @@
+public class Payroll 
+    {
+
+    private static double TotalPayout;  
+    private double Hours;40
+    private double Hourly_Rate;
+    private taxes owned 
+    public static double Salary;
+    private double increaseHours = 10;
+
+public Payroll (String hourly wage , taxes owed and hours worked this week.
+getHourly_Rate)
+    {
+    Hourly Wage;
+    Hours = getHours;
+    Hourly_Rate = getHourly_Rate;
+    Salary = Hours * Hourly_Rate;
+
+    TotalPayout = TotalPayout + Salary;
+    }
