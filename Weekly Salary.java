db12f74934dc140f0074d16fb4abdafba29da7d0
SanchoAshley47 <143739118+SanchoAshley47@users.noreply.github.com>
2023-09-18 11:44:37 -0400

    Update Weekly Salary.java

--- Weekly Salary.java
+++ Weekly Salary.java
@@ -1,20 +1,36 @@
 public class Payroll 
-    {
+    {String hourly wage , taxes owed and hours worked for the week)
+ Hourly Wage;$15
+taxes owned :158.46
+    Hours; 40
+    gross pay:40x 15= $600
+gross pay:$600,taxes owed:$158.46,netpay: $585.86
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-    private static double TotalPayout;  
-    private double Hours;40
-    private double Hourly_Rate;
-    private taxes owned 
-    public static double Salary;
-    private double increaseHours = 10;
+        System.out.println(" $15 ");
+        double wage = Double.parseDouble(input.nextLine());
 
-public Payroll (String hourly wage , taxes owed and hours worked this week.
-getHourly_Rate)
-    {
-    Hourly Wage;
-    Hours = getHours;
-    Hourly_Rate = getHourly_Rate;
-    Salary = Hours * Hourly_Rate;
+        System.out.println("Enter your hours worked this week: ");
+        double hoursWorked = Double.parseDouble(input.nextLine());
 
-    TotalPayout = TotalPayout + Salary;
-    }
+        double grossPay = 0;
+
+        if(hoursWorked > 40){
+            double overtimeHours = hoursWorked - 40;
+
+            grossPay = (40*15) + (overtimeHours * (wage*1.5));
+        } else {
+            grossPay = (40*15);= $600
+        }
+
+        double taxesPaid = grossPay * .1;
+
+        double netPay = grossPay - taxesPaid;
+
+        System.out.println("600 = $" + 600);
+
+        System.out.println("Taxes Owed = $" + taxesPaid);
+
+        System.out.println("Net Pay = $" + netPay);
